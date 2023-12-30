844bfdf29ad943ed93594c8b5fd0ec55ef5d8ebe
ABeGood <openzzggl@gmail.com>
2023-12-30 16:01:32 +0100

    impoved compressor layout

--- static/compressor_script.js
+++ static/compressor_script.js
@@ -1,13 +1,23 @@
 // This script can be used to add interactivity to the page
 // For example, handling the "Add Panel" button click to clone the settings panel
 
-// document.querySelector('.add-panel').addEventListener('click', () => {
-//     const panel = document.querySelector('.settings-panel');
-//     const clone = panel.cloneNode(true);
-//     panel.parentNode.insertBefore(clone, panel.nextSibling);
-// });
 function addPanel() {
     var settingsContainer = document.querySelector('.settings-container');
-    var newPanel = settingsContainer.firstElementChild.cloneNode(true);
+    // Assuming the first child of settingsContainer is the settings panel template
+    var panelTemplate = settingsContainer.getElementsByClassName('settings-panel')[0];
+    var newPanel = panelTemplate.cloneNode(true);
+    // Set up the delete button event handler in the cloned panel
+    newPanel.querySelector('.delete-panel').onclick = function() { deletePanel(this); };
     settingsContainer.appendChild(newPanel);
+}
+
+function deletePanel(button) {
+    var settingsContainer = document.querySelector('.settings-container');
+    var panelToRemove = button.closest('.settings-panel');
+    settingsContainer.removeChild(panelToRemove);
+}
+
+function downloadAll() {
+    // Logic to handle the download of all items
+    alert("Downloading all items..."); // Placeholder action
 }
