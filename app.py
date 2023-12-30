b521a2057829cac76f08c2907b80996c469977f9
ABeGood <openzzggl@gmail.com>
2023-12-30 18:30:36 +0100

    Working output

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect, send_file, jsonify
 from PIL import Image
 import os
 import shutil
@@ -14,8 +14,8 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def compress_image(input_path, output_path, quality=85, max_size=None):
-    img = Image.open(input_path)
+def compress_image(file_name, output_path, quality=85, max_size=None):
+    img = Image.open(file_name)
 
     if img.mode == 'RGBA':
         img = img.convert('RGB')
@@ -26,11 +26,13 @@ def compress_image(input_path, output_path, quality=85, max_size=None):
         while os.path.getsize(output_path) > (max_size * 1000):
             quality = int(quality * 0.9)
             img.save(output_path, 'JPEG', quality=quality, optimize=True)
+        return
 
 def create_archive(files, archive_name):
     with zipfile.ZipFile(archive_name, 'w') as zipf:
         for file in files:
             zipf.write(file, os.path.basename(file))
+            
 
 @app.route('/')
 def index():
@@ -52,28 +54,29 @@ def upload():
     else:
         return "Invalid file format."
 
-@app.route('/compress', methods=['POST', 'GET'])
+@app.route('/compress', methods=['POST'])
 def compress():
-    filename = request.form.get('filename')
-    formats = request.form.getlist('formats')
-    limits = {format: int(request.form.get(f'{format}_limit')) for format in formats}
 
-    compressed_files = []
+    if request.is_json:
+        args = request.get_json()
+        filename = args.get('filename')
+        format = args.get('format')
+        limit = args.get('size')
 
-    for format in formats:
+
         output_folder = os.path.join(app.config['UPLOAD_FOLDER'], format)
         os.makedirs(output_folder, exist_ok=True)
 
         output_file = os.path.join(output_folder, f'{os.path.basename(filename).rsplit(".", 1)[0]}.{format}')
 
-        compress_image(filename, output_file, max_size=limits.get(format))
 
-        compressed_files.append(output_file)
+        filename = filename[1:].replace('/', '\\')
+        compress_image(filename, output_file, max_size=int(limit))
+        # extension = filename.split('.')[-1]
 
-    archive_name = os.path.join(app.config['UPLOAD_FOLDER'], 'compressed_files.zip')
-    create_archive(compressed_files, archive_name)
-
-    return send_file(archive_name, as_attachment=True)
+        return send_file(output_file, as_attachment=True, mimetype=f'image/{format}')
+    else:
+        return jsonify({'status': 'error', 'message': 'Invalid content type'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/compressor_script.js
+++ static/compressor_script.js
@@ -17,6 +17,61 @@ function deletePanel(button) {
     settingsContainer.removeChild(panelToRemove);
 }
 
+async function downloadImage(buttonElement) {
+    // Find the nearest parent settings panel
+    var settingsPanel = buttonElement.closest('.settings-panel');
+
+    // Extract values for format and size from the specific settings panel
+    var format = document.getElementById('format').value;
+    var size = document.getElementById('size').value;
+    var filename = document.getElementById('filename').value;
+
+    // Prepare the data to be sent in the POST request
+    var data = {
+        'filename': filename,
+        'format': format,
+        'size': size
+    };
+
+    // Send the POST request to the Flask endpoint
+    try {
+        const response = await fetch('/compress', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        // Check if the response is JSON or an image
+        const contentDisposition = response.headers.get('Content-Disposition');
+        if (contentDisposition && contentDisposition.indexOf('attachment') !== -1) {
+            // Handle image file response
+            response.blob().then(blob => {
+                // Create a URL for the blob
+                const url = window.URL.createObjectURL(blob);
+                // Create a link and set the URL as the href
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = 'downloaded-image'; // Set a filename for the download
+                document.body.appendChild(a);
+                a.click(); // Trigger the download
+                window.URL.revokeObjectURL(url); // Clean up
+            });
+        } else {
+            // Handle JSON response
+            const data = await response.json();
+            console.log('Success:', data);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
 function downloadAll() {
     // Logic to handle the download of all items
     alert("Downloading all items..."); // Placeholder action
